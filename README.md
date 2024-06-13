e00e930e64bd60550a8ac8042a10cb4772d1d046
Ilgizar <iiahmetzyanov@sbercloud.ru>
2024-06-13 15:37:44 +0300

    Refactoring

--- README.md
+++ README.md
@@ -1,31 +1,30 @@
 ![Python 3.12.3](https://img.shields.io/badge/Python-3.12.3-blue)
 
 # Телеграм бот "Угадай число!"
-Телеграм бот написан на python 3.12.3, использует telegram webhook.
+Телеграм бот написан на python 3.12.3, используется telegram webhook.
 
-Для запуска телеграм бота через Container Apps понадобится:
-- Создать телеграм бота через BotFather - получить токен
-- Собрать docker образ и запушить его в Artifact Registry
-- Создать Container App
+Для запуска телеграм бота через Container Apps необходимо:
+- Получить токен для телеграм бота - написать @BotFather
+- Собрать и залить docker образ в Artifact Registry
+- Создать Container Apps
 - Добавить webhook в telegram  
 
 
 ## Создание Container App
 При создании Container App необходимо:
-1. Указать название
-2. Включить создание публичного адреса (используется для webhook)
-3. Выбрать docker образ с телеграм ботом
-4. `Порт контейнера` указать: 8080
-5. В разделе `Переменные` добавить: BOT_TOKEN=`токен_из_bot_father` 
-6. Конфигурацию можно поставить `мин кол-во экземпляров` в 0
+1. Включить создание публичного адреса (будет использоваться для webhook)
+2. Выбрать docker образ с телеграм ботом
+3. Указать `Порт контейнера`: 8080
+4. В разделе `Переменные` добавить: BOT_TOKEN=`токен_из_bot_father` 
+5. `Мин кол-во экземпляров` можно ставить в 0 (cold start)
 
 ### Добавление webhook в telegram 
-Для работы telegram bot необходимо добавить webhook.
-Заменить:
-- `{BOT_TOKEN}` - поставить токен телеграм бота
-- `{PUBLIC_URL}` - поставить публичный адрес
+Чтобы bot мог получать сообщения из telegram, небходимо добавить webhook:
+- Вместо `{BOT_TOKEN}` - поставить токен телеграм бота
+- Вместо `{PUBLIC_URL}` - поставить публичный адрес
 
 Шаги:
+
 1. Проверить существуют ли webhook: `https://api.telegram.org/bot{BOT_TOKEN}/getWebhookInfo`
 2. Если есть webhook, то удалить: `https://api.telegram.org/bot{BOT_TOKEN}/setWebhook`
-3. Добавить новый webhook: `https://api.telegram.org/bot{BOT_TOKEN}/setWebhook?url=https://{PUBLIC_URL}/{YOUR-TOKEN}/`
+3. Добавить новый webhook: `https://api.telegram.org/bot{BOT_TOKEN}/setWebhook?url={PUBLIC_URL}/{YOUR-TOKEN}/`
--- app/main.py
+++ app/main.py
@@ -5,13 +5,9 @@ from os import getenv
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
 from aiohttp import web
-
-from const import (
-    GUESS_NUMBER_IS_GREATER,
-    GUESS_NUMBER_IS_LESS,
-    USER_GUESSED_MESSAGE,
-    WELCOME_MESSAGE,
-)
+from const import (GUESS_NUMBER_IS_GREATER_MESSAGE,
+                   GUESS_NUMBER_IS_LESS_MESSAGE, USER_GUESSED_MESSAGE,
+                   WELCOME_MESSAGE)
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
@@ -27,17 +23,20 @@ bot = Bot(token=BOT_TOKEN)
 
 dp = Dispatcher()
 
+# API routess
+routes = web.RouteTableDef()
+
 
 @dp.message(CommandStart())
 async def command_start_handler(msg: types.Message) -> None:
-    """При команде /start создаем число"""
+    """Обработчик команды /start"""
 
     generate_user_number(msg=msg)
     await msg.answer(text=WELCOME_MESSAGE)
 
 
 def generate_user_number(msg: types.Message) -> int:
-    """Создаем и возвращаем рандомное число"""
+    """Возвразаем рандомное число"""
 
     number = random.randint(1, 100)
     logger.info(f"Сгененировано число: {number}")
@@ -47,7 +46,7 @@ def generate_user_number(msg: types.Message) -> int:
 
 
 def get_guess_number(msg: types.Message) -> int:
-    """Возвращаем загаданное число. Если нет - генерим новый."""
+    """Возвращаем загаданное число. Если нет - создаем новый"""
 
     guess_number = users_data_db.get(msg.chat.id, None)
     if guess_number is None:
@@ -58,23 +57,36 @@ def get_guess_number(msg: types.Message) -> int:
 
 @dp.message()
 async def message_handler(msg: types.Message) -> None:
+    """Обработчик сообщений"""
     try:
         number = int(msg.text)
         logger.info(f"Пользователь ввел число: {number}")
 
         guess_number = get_guess_number(msg=msg)
         if number > guess_number:
-            await msg.answer(GUESS_NUMBER_IS_LESS)
+            await msg.answer(GUESS_NUMBER_IS_LESS_MESSAGE)
         elif number < guess_number:
-            await msg.answer(GUESS_NUMBER_IS_GREATER)
+            await msg.answer(GUESS_NUMBER_IS_GREATER_MESSAGE)
         else:
             await msg.answer(USER_GUESSED_MESSAGE)
     except TypeError:
         await msg.answer("Введите целое число!")
 
 
-async def handle_webhook(request):
-    """Обрабатываем webhook и шлем сообщению боту"""
+# ----------------
+# API
+# ----------------
+
+
+@routes.get("/")
+async def health(request):
+    """Используется для readiness_probe"""
+    return web.Response(text="OK")
+
+
+@routes.post(f"/{BOT_TOKEN}")
+async def handle_webhook_request(request):
+    """Используется для обработки webhook из telegram"""
 
     # Достаем токен
     url = str(request.url)
@@ -87,7 +99,7 @@ async def handle_webhook(request):
         update = types.Update(**request_data)
         await dp._process_update(bot=bot, update=update)
 
-        return web.Response()
+        return web.Response(text="OK")
     else:
         return web.Response(status=403)
 
@@ -96,5 +108,5 @@ if __name__ == "__main__":
     # Запуск aiohttp сервера
 
     app = web.Application()
-    app.router.add_post(f"/{BOT_TOKEN}", handle_webhook)
+    app.add_routes(routes)
     web.run_app(app, host="0.0.0.0", port=8080)
